7cf1595d80e9f5999f9fea8df3eaaae34dfbdbf7
Angelito <litsdionisio@gmail.com>
2024-11-27 18:07:15 +0800

    Code indentation

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -6,7 +6,7 @@ const ViewAllJobs = () => {
           className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
           >View All Jobs</a
         >
-      </section>
+    </section>
   )
 }
 
